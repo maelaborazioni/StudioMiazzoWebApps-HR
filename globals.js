4afc01e90333edc5c2d3450f7056901c14e2ac10
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-02-05 16:28:51 +0100

    First commit

---
+++ globals.js
@@ -0,0 +1,18 @@
+/**
+ * Callback method for when solution is opened.
+ * 
+ * @param {Array} args
+ *
+ * @properties={typeid:24,uuid:"BFC5D5DB-017E-465D-9356-24286778A01A"}
+ */
+function ma_hr_onSolutionOpen(args) 
+{
+// TODO verificare se possibile eliminarla... il riferimento ad HR_DEMO è sicuramente obsoleto
+//	globals.svy_nav_onOpen(args);
+//	var groupsArray = globals.datasetToArray(security.getUserGroups(globals.svy_sec_user_id), 2);
+//	if(		globals.svy_sec_username && globals.svy_sec_username === globals.DEMO_USER
+//		||	globals.contains(groupsArray, globals.DEMO_GROUP))
+//	{
+//		databaseManager.switchServer(globals.Server.MA_HR, globals.Server.MA_HR_DEMO);
+//	}
+}
